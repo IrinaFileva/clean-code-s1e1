8d6c6dc439dd6dde7c41e64cfe504d6442b24ed7
Irina Fileva <irinkaskai@gmail.com>
2024-01-05 17:38:57 +0500

    refactor(extended-1.2): add alt attribute for images

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button button-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='Remove';
     deleteButtonImg.className="button-img";
     deleteButton.appendChild(deleteButtonImg);
 
